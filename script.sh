b21cb2706f5fe37f388f8113e2e3153468dbc09f
Cpm_jhon <130876841+CpmJhon@users.noreply.github.com>
2024-09-28 05:11:19 +0700

    Delete script.sh

--- script.sh
+++
@@ -1,3 +0,0 @@
-npm start
-curl http://<glitch-project-url>/api
-curl -X POST http://<glitch-project-url>/api/run-lua -H "Content-Type: application/json" -d '{"script": "print(\"Hello from Lua!\")"}'
